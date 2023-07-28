823674f011e15e89337b19ce7d1e4a2b5892db9e
mitesh-arsenaltech <mitesh.ladva@arsenaltech.com>
2023-07-28 12:15:06 +0530

    validation fixed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,15 +2,16 @@ import { Request, Response } from "express";
 import { RegisterValidation } from "../validations/register.validation";
 
 export const register = async ( request : Request, response : Response) =>  {
+
     const body = request.body;
     
-    // const { error } = RegisterValidation.validate(body);
+    const { error } = RegisterValidation.validate(body);
 
-    // error && response.status(400).send(error.details)
+    if (error) return response.status(400).send(error.details);
 
-    // body.password !== body.password_confirm && response.status(400).send({
-    //     message : "Password doesn't match !"
-    // })
+    body.password !== body.password_confirm && response.status(400).send({
+        message : "Password doesn't match !"
+    })
 
     response.send(body);
 };
