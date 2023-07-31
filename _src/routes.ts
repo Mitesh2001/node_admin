9d15001041ba500470d487d126456847afc48ffa
yashbuhecha <yashbuhecha1998@gmail.com>
2023-07-31 21:53:54 +0530

    user CRUD completed

--- _src/routes.ts
+++ _src/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express"
 import { AuthUser, LogOut, Login, Register, UpdateInfo, UpdatePassword } from "./controllers/auth.controller"
 import { AuthMiddleware } from "./middleware/auth.middleware"
-import { CreateUser, DeleteUser, GetUser, UpdateUser, Users } from "./controllers/user.controller"
 
 export const routes = (router : Router) => {
     router.post('/api/register', Register)
@@ -10,10 +9,4 @@ export const routes = (router : Router) => {
     router.get('/api/logout', AuthMiddleware, LogOut)
     router.put('/api/users/info', AuthMiddleware, UpdateInfo)
     router.put('/api/users/password', AuthMiddleware, UpdatePassword)
-
-    router.get('/api/users', AuthMiddleware, Users)
-    router.post('/api/users', AuthMiddleware, CreateUser)
-    router.get('/api/users/:id', AuthMiddleware, GetUser)
-    router.put('/api/users/:id', AuthMiddleware, UpdateUser)
-    router.delete('/api/users/:id', AuthMiddleware, DeleteUser)
 }
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,94 @@
+import { getManager } from "typeorm"
+import { User } from "../entity/user.entity"
+import { Request, Response } from "express"
+import bcryptjs from "bcryptjs"
+
+export const Users = async (request: Request, response: Response) => {
+
+    const userRepository = getManager().getRepository(User)
+
+    const users = await userRepository.find()
+
+    response.send(users.map(user => {
+        delete user.password
+        return user
+    }))
+
+}
+
+export const CreateUser = async (request: Request, response: Response) => {
+
+    const { role_id, ...body } = request.body
+    const hasedPassword = await bcryptjs.hash("secret", 10)
+
+    const userRepository = getManager().getRepository(User)
+
+    const { password, ...user } = await userRepository.save({
+        ...body,
+        password: hasedPassword
+    })
+
+    response.send(user)
+
+}
+
+export const GetUser = async (request: Request, response: Response) => {
+
+    try {
+
+        const userRepository = getManager().getRepository(User)
+
+        const { password, ...user } = await userRepository.findOne({ where: { id: parseInt(request.params.id) } })
+
+        response.send(user)
+
+    } catch (error) {
+        response.send({
+            message: "Something went wrong !"
+        })
+    }
+
+}
+
+export const UpdateUser = async (request: Request, response: Response) => {
+
+    try {
+
+        const userRepository = getManager().getRepository(User)
+
+        await userRepository.update(request.params.id, request.body)
+
+        const { password, ...user } = await userRepository.findOne({ where: { id: parseInt(request.params.id) } })
+
+        response.send(user)
+
+    } catch (error) {
+
+        response.send({
+            message: "Something went wrong !"
+        })
+
+    }
+}
+
+export const DeleteUser = async (request: Request, response: Response) => {
+
+    try {
+
+        const userRepository = getManager().getRepository(User)
+
+        await userRepository.delete(request.params.id)
+
+        response.send({
+            message: "Success !"
+        })
+
+    } catch (error) {
+
+        response.send({
+            message: "Something went wrong !"
+        })
+
+    }
+
+}
